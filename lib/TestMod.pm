b85e0ba9900ba0a7c5ca9771eead984c029938bf
Joelle Maslak <jmaslak@antelope.net>
2018-09-14 17:56:12 -0600

    Initial version

---
+++ lib/TestMod.pm
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+#
+# Copyright (C) 2018 Joelle Maslak
+# All Rights Reserved - See License
+#
+
+# ABSTRACT: Test Module
+
+package TestMod;
+
+use JTM::Boilerplate 'class';
+
+our $VERSION = 1.0000;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+
